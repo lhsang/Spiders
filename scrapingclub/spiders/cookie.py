75a220102670da659e386d5a5ba6bef1da79e050
lhsang <sang.lh@teko.vn>
2019-10-12 13:55:14 +0700

    get and set cookie for next request

--- scrapingclub/spiders/cookie.py
+++ scrapingclub/spiders/cookie.py
@@ -1,15 +1,27 @@
 import scrapy
 import json
 
+from . import getParamsFromCookie, getCookieFromResponse
+
+
 class FindGoldInCookie(scrapy.Spider):
     name = "find_gold_in_cookie"
     file_name = "data/find_gold_in_cookie.json"
     start_urls = ["https://scrapingclub.com/exercise/detail_cookie/"]
-    cookie = None
+    data_url = "https://scrapingclub.com/exercise/ajaxdetail_cookie/"
+    token = None
+    headers = {
+        'referer': 'https://scrapingclub.com/',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 
     def parse(self, response):
-        if not self.cookie:
-            self.cookie = str(response.headers.getlist('Set-Cookie')[0]).split(';')
-            print("============")
-            print(self.cookie)
-            print("=======")
+        if not self.token:
+            params = getParamsFromCookie(getCookieFromResponse(response), ['token'])
+            self.token = params['token']
+            request = scrapy.Request(url=self.data_url + '?token={}'.format(self.token), headers=self.headers)
+            return request
+        else:
+            f = open(self.file_name, "w")
+            f.write(json.dumps(json.loads(response.body)))
+            f.close()
---
+++ scrapingclub/spiders/utils.py
@@ -0,0 +1,24 @@
+import re
+
+
+def getParamsFromCookie(cookie, params):
+    """
+    get params from cookie
+    :param cookie: cookie
+    :param params: list name of params which you want
+    :return: dict result
+    """
+    ans = {}
+    for param in params:
+        value = re.findall(r'{}=[a-zA-Z0-9]+;'.format(param), cookie)[0]
+        ans[param] = value.split('=')[1][:-1]
+    return ans
+
+
+def getCookieFromResponse(response):
+    """
+    get cookie from response
+    :param response: response after request
+    :return: string cookie
+    """
+    return str(response.headers.getlist('Set-Cookie')[0])
